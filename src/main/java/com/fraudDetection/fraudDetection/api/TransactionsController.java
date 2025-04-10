b0508cd51555bf2d2cdfd5fe5bbfcbe969f2a3b4
Mburu-Elvis <mburu.elvis1@students.jkuat.ac.ke>
2025-04-10 05:38:31 +0300

    project initialization and kafka configuration

---
+++ src/main/java/com/fraudDetection/fraudDetection/api/TransactionsController.java
@@ -0,0 +1,21 @@
+package com.fraudDetection.fraudDetection.api;
+
+import com.fraudDetection.fraudDetection.config.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/api/v1")
+public class TransactionsController {
+    @Autowired
+    private MessageProducer messageProducer;
+
+    @PostMapping("/transactions")
+    public String processTransaction(@RequestParam("message") String message) {
+        messageProducer.sendMessage("fraud-detection", message);
+        return "Message sent: " + message;
+    }
+}
